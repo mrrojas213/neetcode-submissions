fb9283d25928d2cb186bca0ab0fc8bec7877d3b2
Jose Rojas <mrrojas213@gmail.com>
2026-05-19 15:34:50 -0500

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.java
@@ -0,0 +1,31 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> MinStack;
+    public MinStack() {
+        stack = new Stack<>();
+        MinStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(MinStack.isEmpty()){
+            MinStack.push(val);
+        } else{
+            MinStack.push(Math.min(MinStack.peek(), val));
+        }
+        
+    }
+    
+    public void pop() {
+        stack.pop();
+        MinStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return MinStack.peek();
+    }
+}
